17fd5902d6116f0c654a7348b1ee91df65730587
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-13 13:51:06 -0600

    Add methods to format search results.
    Co-authored-by: Kwibe Merci <46441816+jKwibe@users.noreply.github.com>
    Co-authored-by: Taija Warbelow 
    <10294841+twarbelow@users.noreply.github.com>
    Co-authored-by: Eric Hale <59599782+EHale64@users.noreply.github.com>
    Co-authored by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,34 @@ class ApplicationController < Sinatra::Base
     JSON.generate(message: 'Hello Practice Hackers!')
   end
 
+  get '/api/v1/search' do
+
+    data = Faraday.get("https://api.openopus.org/omnisearch/#{params[:q]}/0.json")
+
+    parsed_data = JSON.parse(data.body, symbolize_names: true)
+    info = {
+        next: parsed_data[:next],
+        results: []
+    }
+
+    parsed_data[:results].each do |result|
+      single_results = {
+          composer: {},
+          work: {}
+      }
+      single_results[:composer][:name] = result[:composer][:complete_name]
+      single_results[:composer][:id] = result[:composer][:id]
+      if result[:work].nil?
+        single_results[:work][:title] = nil
+      else
+        single_results[:work][:title] = result[:work][:title]
+        single_results[:work][:id] = result[:work][:id]
+        single_results[:work][:subtitle] = result[:work][:subtitle]
+      end
+
+      info[:results] << single_results
+    end
+    info.to_json
+  end
+
 end
