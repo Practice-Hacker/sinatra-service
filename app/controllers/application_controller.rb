7e91b4a3ce76fbf9966411ff8935a5bf5575eb12
twarbelow <taija.warbelow@google.com>
2020-09-15 18:48:30 -0700

    use map instead of each. style fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,36 +13,33 @@ class ApplicationController < Sinatra::Base
   get '/api/v1/search' do
 
     data = Faraday.get("https://api.openopus.org/omnisearch/#{params[:q]}/0.json")
-
     parsed_data = JSON.parse(data.body, symbolize_names: true)
-    info = {
+
+    response = {
         next: parsed_data[:next],
-        results: []
-    }
-
-    parsed_data[:results].each do |raw|
-      result = {
-          composer: {
-            name: nil,
-            id: nil
-          },
-          work: {
-            title: nil,
-            subtitle: nil,
-            id: nil
+        results: parsed_data[:results].map do |raw|
+          result = {
+              composer: {
+                name: nil,
+                id: nil
+              },
+              work: {
+                title: nil,
+                subtitle: nil,
+                id: nil
+              }
           }
-      }
-      result[:composer][:name] = raw[:composer][:complete_name]
-      result[:composer][:id] = raw[:composer][:id]
-      unless raw[:work].nil?
-        result[:work][:title] = raw[:work][:title]
-        result[:work][:id] = raw[:work][:id]
-        result[:work][:subtitle] = raw[:work][:subtitle]
-      end
-
-      info[:results] << result
-    end
-    info.to_json
+          result[:composer][:name] = raw[:composer][:complete_name]
+          result[:composer][:id] = raw[:composer][:id]
+          unless raw[:work].nil?
+            result[:work][:title] = raw[:work][:title]
+            result[:work][:id] = raw[:work][:id]
+            result[:work][:subtitle] = raw[:work][:subtitle]
+          end
+          result
+        end
+    }
+    
+    response.to_json
   end
-
 end
